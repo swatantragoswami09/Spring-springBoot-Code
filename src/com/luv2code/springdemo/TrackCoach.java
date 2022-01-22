a77687e79873a4c09b67c27e2d24ae6fc1619d30
NAGARRO\swatantragoswami <swatantra.goswami_cs17@nagarro.com>
2022-01-22 18:52:10 +0530

    beanlifeCycle ilustration

--- src/com/luv2code/springdemo/TrackCoach.java
+++ src/com/luv2code/springdemo/TrackCoach.java
@@ -21,4 +21,19 @@ public class TrackCoach implements Coach{
     public String getDailyFortune() {
          return "Just DO It: "+fortuneService.getFortune();
     }
+
+    // add an init method
+    public void doMyStartupStuff() {
+        System.out.println("TrackCoach: inside method doMyStartupStuff");
+    }
+    // add a destroy method
+    public void doMyCleanupStuffYoYo() {
+        
+        System.out.println("TrackCoach: inside method doMycleanupStuffYoYo");
+    }
+    
+    
+
+
+
 }
